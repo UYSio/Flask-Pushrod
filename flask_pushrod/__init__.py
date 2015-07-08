e28db695272759eec968c065b786163b9ac4b15d
Juan Matthys Uys <opyate@gmail.com>
2015-07-08 13:21:52 +0100

    todo for v0.3.1; clean up setup.py

--- flask_pushrod/__init__.py
+++ flask_pushrod/__init__.py
@@ -2,3 +2,5 @@ from .resolver import Pushrod, pushrod_view
 from .renderers import UnrenderedResponse
 
 from . import renderers, resolver
+
+__version__ = '0.3.1'
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
+import codecs
 from os import path
+import re
 
 
 class PyTest(TestCommand):
@@ -15,16 +17,32 @@ class PyTest(TestCommand):
         import pytest
         raise SystemExit(pytest.main(self.test_args))
 
+here = path.abspath(path.dirname(__file__))
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    return codecs.open(path.join(here, *parts), 'r').read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+long_description = read('README.rst')
+
 
 setup(
     name='Flask-Pushrod',
-    version='0.3',
+    version=find_version('flask_pushrod', '__init__.py'),
     url='http://github.com/UYSio/flask-pushrod',
     license='MIT',
     author='Juan Matthys Uys',
     author_email='opyate+flaskpushrod@gmail.com',
     description='Views for your API',
-    long_description=open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')).read(),
+    long_description=long_description,
     packages=['flask_pushrod', 'flask_pushrod.renderers'],
     zip_safe=False,
     platforms='any',
